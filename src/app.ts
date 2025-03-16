4194e9e3e5b5303975b95dfe78ad47f673a36abe
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2025-03-16 22:45:22 +0900

    feat: Input tag에 있는 value 확인하기 + 이벤트 등록(binding을 활용)

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,12 @@ class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
 
     constructor() {
+        // document에서 검색하므로 getElementById를 통해 검색(document 객체에서 ID를 기준으로 즉시 검색하여 최적화)
         const templateEl = document.getElementById("project-input");
         const hostEl = document.getElementById("app");
 
@@ -17,7 +21,25 @@ class ProjectInput {
 
         const importedHtmlContent = document.importNode(this.templateElement.content, true);
         this.element  = importedHtmlContent.firstElementChild as HTMLElement;
+        this.element.id = "user-input";
+
+        // 범위가 좁혀졌으니 querySelector를 통해 가독성 향상
+        this.titleInputElement = this.element.querySelector("#title")! as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector("#people")! as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector("#description")! as HTMLInputElement;
+
+        this.configure();
         this.attach();
+    }
+
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
+    }
+
+    private configure() {
+        // binding을 하지 않으면 콜백함수를 호출하는 친구는 this.element이기에 submitHandler의 this는 this.element가 된다.
+        this.element.addEventListener("submit", this.submitHandler.bind(this));
     }
 
     private attach() {
